7008daf86407b63a66162edbc3c9b38b15fcc0e4
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-05-09 14:49:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,19 +8,13 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def random_answer(message):
-    bot.send_message(chat_id=message.chat.id, text=f"Hello👋 \n\n🗳Get latest Movies from 1Tamilmv and 1Tamilblasters\n\n⚙️*How to use me??*🤔\n\n✯ Please Enter */view_tamilmv* or */view_tamilblasters* command and you'll get magnet link as well as link to torrent file 😌\n\nShare and Support💝", parse_mode='Markdown')
+    bot.send_message(chat_id=message.chat.id, text=f"Hello👋 \n\n🗳Get latest Movies from 1Tamilmv\n\n⚙️*How to use me??*🤔\n\n✯ Please Enter */view* command and you'll get magnet link as well as link to torrent file 😌\n\nShare and Support💝", parse_mode='Markdown')
 
-@bot.message_handler(commands=['view_tamilmv'])
-def start_tamilmv(message):
+@bot.message_handler(commands=['view'])
+def start(message):
     bot.send_message(message.chat.id, text="*Please wait...*", parse_mode='Markdown')
     tamilmv()
-    bot.send_message(chat_id=message.chat.id, text="Select a Movie from 1Tamilmv 🙂 :", reply_markup=makeKeyboard(), parse_mode='HTML')
-
-@bot.message_handler(commands=['view_tamilblasters'])
-def start_tamilblasters(message):
-    bot.send_message(message.chat.id, text="*Please wait...*", parse_mode='Markdown')
-    tamilblasters()
-    bot.send_message(chat_id=message.chat.id, text="Select a Movie from 1Tamilblasters 🙂 :", reply_markup=makeKeyboard(), parse_mode='HTML')
+    bot.send_message(chat_id=message.chat.id, text="Select a Movie from the list 🙂 :", reply_markup=makeKeyboard(), parse_mode='HTML')
 
 @bot.callback_query_handler(func=lambda message: True)
 def callback_query(call):
@@ -39,35 +33,6 @@ def makeKeyboard():
 
 def tamilmv():
     mainUrl = 'https://www.1tamilmv.yt/'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
-        'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'Connection': 'Keep-alive',
-        'sec-ch-ua-platform': '"Windows"',
-    }
-    global movie_dict, real_dict, movie_list
-    movie_dict = {}
-    real_dict = {}
-    movie_list = []
-    web = requests.get(mainUrl, headers=headers)
-    soup = BeautifulSoup(web.text, 'lxml')
-    temps = soup.find_all('div', {'class': 'ipsType_break ipsContained'})
-    for i in range(21):
-        title = temps[i].find('a').text.strip()
-        movie_dict[title] = None
-        movie_list.append(title)
-        link = temps[i].find('a')['href']
-        html = requests.get(link)
-        soup = BeautifulSoup(html.text, 'lxml')
-        mag_links = [a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('magnet')]
-        torrent_links = [a['href'] for a in soup.find_all('a', {"data-fileext": "torrent", 'href': True})]
-        titles = [span.text[19:-8] for span in soup.find_all('span') if span.text.endswith('torrent')]
-        for p in range(len(mag_links)):
-            real_dict.setdefault(movie_list[i], []).append(f"*{titles[p]}* -->\n🧲 `{mag_links[p]}`")
-
-def tamilblasters():
-    mainUrl = 'https://1tamilblasters.pm/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
         'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
